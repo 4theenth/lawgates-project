bedc1a34ca405f43aeb74e6fa8c76ee589115784
yudis <yudistirapurba05@gmail.com>
2026-09-08 11:44:08 +0800

    Fix: fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,12 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            JenisPeraturanSeeder::class,
-            StatusPeraturanSeeder::class,
-            RelationTypeSeeder::class,
-            PerubahanTypeSeeder::class,
-            KategoriHukumSeeder::class,
-        ]);
+        //
     }
 }
--- database/seeders/JenisPeraturanSeeder.php
+++ database/seeders/JenisPeraturanSeeder.php
@@ -10,15 +10,42 @@ class JenisPeraturanSeeder extends Seeder
     public function run(): void
     {
         DB::table('jenis_peraturan')->insert([
-            ['kode' => 'UUD', 'nama' => 'Undang-Undang Dasar 1945', 'urutan_hierarki' => 1],
-            ['kode' => 'TAP MPR', 'nama' => 'Ketetapan MPR', 'urutan_hierarki' => 2],
-            ['kode' => 'UU', 'nama' => 'Undang-Undang', 'urutan_hierarki' => 3],
-            ['kode' => 'PERPU', 'nama' => 'Peraturan Pemerintah Pengganti Undang-Undang', 'urutan_hierarki' => 3],
-            ['kode' => 'PP', 'nama' => 'Peraturan Pemerintah', 'urutan_hierarki' => 4],
-            ['kode' => 'PERPRES', 'nama' => 'Peraturan Presiden', 'urutan_hierarki' => 5],
-            ['kode' => 'PERDA PROV', 'nama' => 'Peraturan Daerah Provinsi', 'urutan_hierarki' => 6],
-            ['kode' => 'PERDA KAB', 'nama' => 'Peraturan Daerah Kabupaten/Kota', 'urutan_hierarki' => 7],
-            ['kode' => 'PERMEN', 'nama' => 'Peraturan Menteri', 'urutan_hierarki' => 8],
+            [
+                'kode' => 'UUD',
+                'nama' => 'Undang-Undang Dasar 1945',
+            ],
+            [
+                'kode' => 'TAP MPR',
+                'nama' => 'Ketetapan MPR',
+            ],
+            [
+                'kode' => 'UU',
+                'nama' => 'Undang-Undang',
+            ],
+            [
+                'kode' => 'PERPU',
+                'nama' => 'Peraturan Pemerintah Pengganti Undang-Undang',
+            ],
+            [
+                'kode' => 'PP',
+                'nama' => 'Peraturan Pemerintah',
+            ],
+            [
+                'kode' => 'PERPRES',
+                'nama' => 'Peraturan Presiden',
+            ],
+            [
+                'kode' => 'PERDA PROV',
+                'nama' => 'Peraturan Daerah Provinsi',
+            ],
+            [
+                'kode' => 'PERDA KAB',
+                'nama' => 'Peraturan Daerah Kabupaten/Kota',
+            ],
+            [
+                'kode' => 'PERMEN',
+                'nama' => 'Peraturan Menteri',
+            ],
         ]);
     }
 }
