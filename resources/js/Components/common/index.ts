662889cab27d1486122ec980f0a1f7da0dc73464
Monica Agustriani Purba <monicapurba22@gmail.com>
2026-09-11 16:47:09 +0800

    feat(common): add status badge components

---
+++ resources/js/Components/common/index.ts
@@ -0,0 +1,14 @@
+export { default as ApplicationLogo } from './ApplicationLogo';
+export { default as Checkbox } from './Checkbox';
+export { CountUp } from './CountUp';
+export { default as DangerButton } from './DangerButton';
+export { default as Dropdown } from './Dropdown';
+export { default as InputError } from './InputError';
+export { default as InputLabel } from './InputLabel';
+export { default as Modal } from './Modal';
+export { default as NavLink } from './NavLink';
+export { default as PrimaryButton } from './PrimaryButton';
+export { default as ResponsiveNavLink } from './ResponsiveNavLink';
+export { default as SecondaryButton } from './SecondaryButton';
+export { default as TextInput } from './TextInput';
+export { StatusBadge } from './StatusBadge';
