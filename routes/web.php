0e820b73d6178d5031f2c4d7d6856e933d325f85
Monica Agustriani Purba <monicapurba22@gmail.com>
2026-09-11 16:39:20 +0800

    feat(routes): add admin routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('admin.dokumen-hukum');
+    });
+    Route::get('/dashboard', function () {
+        return redirect()->route('admin.dokumen-hukum');
+    })->name('admin.dashboard');
+    Route::get('/dokumen-hukum', function () {
+        return Inertia::render('Admin/DokumenHukum/Index');
+    })->name('admin.dokumen-hukum');
+    Route::get('/dokumen-hukum/tambah', function () {
+        return Inertia::render('Admin/DokumenHukum/Create');
+    })->name('admin.dokumen-hukum.tambah');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
