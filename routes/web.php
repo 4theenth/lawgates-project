c2a3a10bb4c8387aa265a4d94a7e17e275d7d655
Monica Agustriani Purba <monicapurba22@gmail.com>
2026-09-22 16:24:50 +0800

    feat(team): add team management route and controller

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,7 @@ Route::get('/api/referensi-filter', [PeraturanController::class, 'referensiFilte
 */
 use App\Http\Controllers\Admin\DokumenHukumController;
 use App\Http\Controllers\Admin\KategoriHukumController;
+use App\Http\Controllers\Admin\TeamController;
 
 
 Route::prefix('admin')->middleware(['auth', 'role:superadmin,admin'])->group(function () {
@@ -84,6 +85,9 @@ Route::prefix('admin')->middleware(['auth', 'role:superadmin,admin'])->group(fun
     // MinIO Sync Routes
     Route::get('/dokumen-hukum/minio/scan', [DokumenHukumController::class, 'scanMinio'])->name('admin.dokumen-hukum.minio.scan');
     Route::post('/dokumen-hukum/minio/import', [DokumenHukumController::class, 'importFromMinio'])->name('admin.dokumen-hukum.minio.import');
+
+    // Users & Team Management Routes
+    Route::get('/users/tim', [TeamController::class, 'index'])->name('admin.users.tim');
 });
 
 /*
